a1de4a7d4a359bb6fd16b3ad59da8e65621367c8
Amjad lal k <227345560+amjadlalkodithodika-web@users.noreply.github.com>
2025-10-08 13:55:15 +0530

    Add files via upload

--- icatopython.py
+++ icatopython.py
@@ -24,34 +24,46 @@ with col2:
             making it both technically robust and business-ready.'""")
 col1,col2,col3=st.columns([1,4,1])
 with col2:
-    age=st.number_input("Enter your age",min_value=10,max_value=99)
+    age = st.number_input(
+    "Enter your age",
+    min_value=10,
+    max_value=99,
+    value=None,
+    placeholder="Enter age..."
+)
+
+    # age=st.number_input("Enter your age",min_value=10,max_value=99)
     annual_income=st.slider("Enter Your Annual Income",min_value=100,max_value=600000,step=100)
     poli_options=['Health', 'Auto', 'Life', 'Property']
-    policy_type=st.selectbox("Enter policy Type",poli_options)
+    policy_type=st.selectbox("Enter policy Type",poli_options,index=None,
+    placeholder="Select a policy type...")
     coverage_amount=st.slider("Expected Coverage",min_value=100,max_value=600000,step=100)
     claim_amount=st.slider("Claim Amount",min_value=100,max_value=200000,step=100)
-    past_claim=st.number_input("Past Claim",min_value=0,max_value=15,step=1)
-    doc_sbmt=st.number_input("How Many Documment Submitted",min_value=0,max_value=15,step=1)
+    past_claim=st.number_input("Past Claim",min_value=0,max_value=15,step=1,value=None,
+    placeholder="Past Claims...")
+    doc_sbmt=st.number_input("How Many Documment Submitted",min_value=0,max_value=15,step=1,value=None,
+    placeholder="Documment's Count...")
     fraud_option=['Low', 'Medium','High']
     fraud_risk=st.select_slider("Fraud Risk",fraud_option)
 col1,col2,col3=st.columns([1,2,3])
 with col2:
     if st.button("Click"):
         try:
-            policy_type = le.transform([policy_type])[0]          # LabelEncoder
-            fraud_risk = oe.transform([[fraud_risk]])[0][0]       # OrdinalEncoder
-        
-            features = [[age, annual_income, policy_type, coverage_amount,
-                        claim_amount, past_claim, doc_sbmt, fraud_risk]]
-        
-            prediction = model.predict(features)
-        
-            if prediction[0] == 1:
-                st.success("✅ Claim Approved: The submitted claim meets policy requirements.")
+            if policy_type is None or fraud_risk is None:
+                st.warning("⚠️ Please Fill the columns before prediction.")
             else:
-                st.error("❌ Claim Not Approved: Please review documentation and claim details.")
+                # Safe to encode now
+                policy_type = le.transform([policy_type])[0]          # LabelEncoder
+                fraud_risk = oe.transform([[fraud_risk]])[0][0]       # OrdinalEncoder
+
+                features = [[age, annual_income, policy_type, coverage_amount,
+                            claim_amount, past_claim, doc_sbmt, fraud_risk]]
+
+                prediction = model.predict(features)
+
+                if prediction[0] == 1:
+                    st.success("✅ Claim Approved: The submitted claim meets policy requirements.")
+                else:
+                    st.error("❌ Claim Not Approved: Please review documentation and claim details.")
         except Exception as e:
             st.error(f'Error in Prediction: {e}')
-
-        
-
